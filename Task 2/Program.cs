a05027bd406395119e330e3fa42121073aec9cfd
lglama <laneelama@gmail.com>
2025-11-04 15:19:14 +0545

    changes

---
+++ Task 2/Program.cs
@@ -0,0 +1,8 @@
+﻿const double PI = 3.14;
+
+double radius = 9;
+double area = PI * radius * radius;
+double perimeter = 2 * PI * radius;
+
+Console.WriteLine($"Area of circle: {area}");
+Console.WriteLine($"Perimeter of circle: {perimeter}");
