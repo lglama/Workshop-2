61feace3d0b416b560ced3169541286c254e99ea
lglama <laneelama@gmail.com>
2025-11-04 15:36:33 +0545

    Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+List<string> fruits = new List<string> { "Strawberry", "Pineapple", "Litchi" };
+
+fruits.Add("Orange");
+
+fruits.Remove("Pineapple");
+
+Console.WriteLine("Fruits in the list:");
+foreach (string fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
+
+Dictionary<int, string> fruitDictionary = new Dictionary<int, string>
+{
+    { 1, "Strawberry" },
+    { 2, "Litchi" },
+    { 3, "Orange" }
+};
+
+fruitDictionary.Add(4, "Grapes");
+
+Console.WriteLine("\nFruits in the dictionary:");
+foreach (var pair in fruitDictionary)
+{
+    Console.WriteLine($"ID: {pair.Key}, Name: {pair.Value}");
+}
